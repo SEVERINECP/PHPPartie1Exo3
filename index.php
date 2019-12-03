fd776ddfc1c3fdf454847b9522843635125ee50e
SEVERINECP <sevpicot@gmail.com>
2019-12-03 11:31:29 +0100

    PHPPartie1Exo3

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$km1 = 1;
+$km2 = 3;
+$km3 = 125;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?= $km1; ?></p>
+    <p><?= $km2; ?></p>
+    <p><?= $km3; ?></p>
+  </body>
+</html>
